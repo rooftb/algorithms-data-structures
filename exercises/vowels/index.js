d4073daa76edd267c0ccdddbdf6bc50f44903d24
Tanner <rooftb@email.sc.edu>
2020-09-15 12:01:31 -0400

    completed more exercises

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,9 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const findVowels = str.match(/[aeiou]/gi);
+  return findVowels ? findVowels.length : 0;
+}
 
 module.exports = vowels;
